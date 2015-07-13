85b89b055061efae17bdfcc2d57ab0c9f660a527
moon <moon@spaaaade.com>
2015-07-13 11:40:10 +0900

    - 게시판 만든다

--- board-list.php
+++ board-list.php
@@ -13,6 +13,102 @@
 
 		<section class="row span_24">
 			<h2>Board List</h2>
+
+
+			<div>
+				<ul class="boardList">
+					<li>
+						<div class="thumb">
+							<a href="#">
+								<img src="images/test.jpg" alt="" width="200px" height="200px"/>
+							</a>
+						</div>
+
+						<div class="text">
+							<a href="#"><strong>Test Item</strong></a>
+							<a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at orci odio.
+								Praesent
+								et rhoncus magna. Nam non elit non ipsum tincidunt dapibus. Quisque lorem felis,
+								fermentum
+								vitae est eget, feugiat rhoncus risus. Morbi bibendum dui nulla, sit amet sagittis massa
+								imperdiet nec. </a>
+						</div>
+					</li>
+					<li>
+						<div class="thumb">
+							<a href="#">
+								<img src="images/test.jpg" alt="" width="200px" height="200px"/>
+							</a>
+						</div>
+
+						<div class="text">
+							<a href="#"><strong>Test Item</strong></a>
+							<a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at orci odio.
+								Praesent
+								et rhoncus magna. Nam non elit non ipsum tincidunt dapibus. Quisque lorem felis,
+								fermentum
+								vitae est eget, feugiat rhoncus risus. Morbi bibendum dui nulla, sit amet sagittis massa
+								imperdiet nec. </a>
+						</div>
+					</li>
+					<li>
+						<div class="thumb">
+							<a href="#">
+								<img src="images/test.jpg" alt="" width="200px" height="200px"/>
+							</a>
+						</div>
+
+						<div class="text">
+							<a href="#"><strong>Test Item</strong></a>
+							<a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at orci odio.
+								Praesent
+								et rhoncus magna. Nam non elit non ipsum tincidunt dapibus. Quisque lorem felis,
+								fermentum
+								vitae est eget, feugiat rhoncus risus. Morbi bibendum dui nulla, sit amet sagittis massa
+								imperdiet nec. </a>
+						</div>
+					</li>
+					<li>
+						<div class="thumb">
+							<a href="#">
+								<img src="images/test.jpg" alt="" width="200px" height="200px"/>
+							</a>
+						</div>
+
+						<div class="text">
+							<a href="#"><strong>Test Item</strong></a>
+							<a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at orci odio.
+								Praesent
+								et rhoncus magna. Nam non elit non ipsum tincidunt dapibus. Quisque lorem felis,
+								fermentum
+								vitae est eget, feugiat rhoncus risus. Morbi bibendum dui nulla, sit amet sagittis massa
+								imperdiet nec. </a>
+						</div>
+					</li>
+				</ul>
+
+				<div>
+					<a href="#" class="button">
+						<span>쓰기</span>
+					</a>
+					<a href="#" class="button">
+						<span>삭제</span>
+					</a>
+				</div>
+
+				<ul class="pagination">
+					<li class="prev"><a href="#">이전</a></li>
+					<li><a href="#">1</a></li>
+					<li class="current"><a href="#">2</a></li>
+					<li><a href="#">3</a></li>
+					<li><a href="#">4</a></li>
+					<li class="next"><a href="#">다음</a></li>
+				</ul>
+			</div>
+
+
+
+
 		</section>
 
 	</div><!-- .contentWidth -->
