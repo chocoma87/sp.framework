c1851bbf9c27385f2bb0994816f92cea73dac508
moon <moon@spaaaade.com>
2015-12-07 18:27:16 +0900

    - 추가한 페이지 서브메뉴에 링크 추가한다.

--- include/lnb_board.php
+++ include/lnb_board.php
@@ -4,6 +4,8 @@
 	</div>
 
 	<ul data-accordion="lnbMenu">
-		<li><a href="elements-typo.php">List</a></li>
+		<li><a href="board-list.php">List</a></li>
+		<li><a href="board-table.php">table</a></li>
+		<li><a href="board-market.php">market</a></li>
 	</ul>
 </nav>
