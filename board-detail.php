fd41cec1e747aa92800e938b323d710b0b05fe24
moon <moon@spaaaade.com>
2015-12-07 18:40:07 +0900

    - 보드 상세페이지 추가한다.

---
+++ board-detail.php
@@ -0,0 +1,31 @@
+
+
+<?php include('include/_header.php'); ?>
+
+
+<div id="contentWrap">
+
+	<div class="contentWidth subpage">
+
+		<div class="lnbWrap col span_6">
+			<?include('include/lnb_board.php');?>
+		</div>
+
+		<section class="col span_18">
+			<h2>Board detail</h2>
+
+
+
+
+
+
+
+		</section>
+
+	</div><!-- .contentWidth -->
+
+
+</div><!-- .contentWrap -->
+
+
+<?php include('include/_footer.php'); ?>
